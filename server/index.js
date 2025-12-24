465d63fdff36ba652d2bfb52ea993d88a861df1e
Rico Lai <157272599+u26jl23@users.noreply.github.com>
2025-12-24 21:25:45 +0800

    Add server logic
    Add logic of server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const http = require("http")
 const cors = require("cors")
 const { Server } = require("socket.io")
+const sessions = {}
 
 const app = express()
 app.use(cors())
@@ -17,6 +18,21 @@ const io = new Server(server, {
 
 io.on("connection", (socket)=> {
     console.log("User connected: ", socket.id)
+    socket.on("create_session", ({type})=>{
+        const sessionId = Math.random().toString(36).substring(2, 8).toUpperCase()
+        sessions[sessionId] = {
+            type, // attendance or qa
+            student: [],
+            answers: [],
+            active: true,
+            question: type === 'qa'? '': null
+        }
+        socket.join(sessionId)
+        socket.emit('session_created', sessionId)
+        console.log(`Session ${sessionId} created (${type})`)
+    })
+
+
     socket.on("disconnect", ()=>{
         console.log("User disconnected:", socket.id)
     })
