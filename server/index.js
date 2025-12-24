028fbab0f3767ae831cb8d86b4d10a04b5b3b279
Rico Lai <157272599+u26jl23@users.noreply.github.com>
2025-12-24 21:12:47 +0800

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const http = require("http")
+const cors = require("cors")
+const { Server } = require("socket.io")
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket)=> {
+    console.log("User connected: ", socket.id)
+    socket.on("disconnect", ()=>{
+        console.log("User disconnected:", socket.id)
+    })
+})
+
+server.listen(3001, () => {
+    console.log("Server running on port 3001")
+})
